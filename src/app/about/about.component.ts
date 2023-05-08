61b41534e3533a08523e934a1c6ff50175144443
jhunemelvin <bagaporobotparts@gmail.com>
2023-05-08 22:14:46 +0800

    initialize

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faCss3 } from '@fortawesome/free-brands-svg-icons';
+import { faSquareJs } from '@fortawesome/free-brands-svg-icons';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-about',
@@ -6,5 +12,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  html = faHtml5;
+  css = faCss3;
+  js = faSquareJs;
+  angular = faAngular;
+  github = faGithub;
 
+  scrollDown(): void {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth'
+    });
+  }
 }
