efa392f8e9ce430dd5cc4f0aef2efca28a145894
jhunemelvin <bagaporobotparts@gmail.com>
2023-05-15 21:47:03 +0800

    initialize

--- src/app/servs/servs.component.ts
+++ src/app/servs/servs.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-servs',
@@ -8,5 +10,5 @@ import { faCode } from '@fortawesome/free-solid-svg-icons';
 })
 export class ServsComponent {
   faCode = faCode;
-
+  penIcon = faPen;
 }
