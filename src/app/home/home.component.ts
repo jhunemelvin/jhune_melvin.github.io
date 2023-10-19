4bcb7b9e355647e4a14fa533c9e4c224dc03915b
jhunemelvin <bagaporobotparts@gmail.com>
2023-10-20 02:21:39 +0800

    auto-type in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   facebookIcon = faFacebookF;
   twitterIcon = faTwitter;
   instagramIcon = faInstagram;
   github = faGithub;
+
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    const options = {
+      strings: ['Front-End Developer'],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true
+    };
+
+    const typed = new Typed(this.el.nativeElement.querySelector('.typewriter'), options);
+  }
 }
